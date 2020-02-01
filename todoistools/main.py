b93d457e3893ae4bfcfbbc855bd4babf46f30281
tadashi-aikawa <syou.maman@gmail.com>
2020-02-01 20:53:49 +0900

    :fire: Remove a randomize option

--- todoistools/main.py
+++ todoistools/main.py
@@ -9,6 +9,8 @@ import fire
 from fn import _
 from typing import List
 
+from owlmixin import TList
+
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(PROJECT_ROOT)
 
@@ -20,7 +22,7 @@ def to_times(task: TodoistApiTask) -> List[str]:
     return [''.join(x.split(':')).zfill(4) for x in re.findall('\d+:\d+', task.content)]
 
 
-def sort(config='config.yml', randomize=False):
+def sort(config='config.yml'):
     conf: Config = Config.from_yamlf(config)
 
     scheduled_tasks, free_tasks = api.fetch_uncompleted_tasks(conf.token, PendulumDate.today())\
@@ -30,10 +32,6 @@ def sort(config='config.yml', randomize=False):
         .order_by(lambda x: to_times(x))\
         .concat(free_tasks.filter(_.labels), first=True)\
         .concat(free_tasks.reject(_.labels), first=False)
-
-    if randomize:
-        print('Sort randomize')
-        random.shuffle(sorted_tasks)
 
     if api.update_day_orders(conf.token, sorted_tasks.map(_.id)):
         print('success')
